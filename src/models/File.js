1779b6590b742bd0f0904ec95561d5798fe1a517
sjoseph7 <17250762+sjoseph7@users.noreply.github.com>
2023-03-26 01:36:45 -0700

    feat: change "path" field to "slug" in file schema
    This is the file's unique name in remote storage

--- src/models/File.js
+++ src/models/File.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const FileSchema = new mongoose.Schema(
   {
-    path: { type: String, required: true },
     groupId: { type: String, required: true },
     name: { type: String, required: true },
+    slug: { type: String, required: true },
     downloadCount: { type: Number, required: true, default: 0 },
   },
   { timestamps: true }
